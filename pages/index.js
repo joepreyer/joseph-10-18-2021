b65d4c51aa5788e155f9c225daa32a92c9996277
Joe Preyer <joepreyer@gmail.com>
2021-10-14 18:06:13 +0100

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
+  const [bids, setBids] = useState([]);
+  const [asks, setAsks] = useState([]);
+
   let aWebSocket;
   if (typeof window !== "undefined") {
     aWebSocket = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
@@ -12,22 +16,30 @@ export default function Home() {
       console.log("WebSocket is closed now.");
     };
     aWebSocket.onmessage = function (event) {
-      console.debug("WebSocket message received:", event);
+      let data = JSON.parse(event.data);
+      const {bids, asks} = data
+      if (data.feed === "book_ui_1_snapshot") {
+        setBids(bids);
+        setAsks(asks);
+      }
+      console.debug("WebSocket message received:", JSON.parse(event.data));
     };
   }
 
   const sendToWebSocket = () => {
     aWebSocket &&
-      aWebSocket.send(JSON.stringify({
-        event: "subscribe",
-        feed: "book_ui_1",
-        product_ids: ["PI_XBTUSD"],
-      }));
+      aWebSocket.send(
+        JSON.stringify({
+          event: "subscribe",
+          feed: "book_ui_1",
+          product_ids: ["PI_XBTUSD"],
+        })
+      );
   };
 
   const closeWebSocket = () => {
-    aWebSocket && aWebSocket.close()
-  }
+    aWebSocket && aWebSocket.close();
+  };
 
   return (
     <div className="container">
@@ -45,6 +57,14 @@ export default function Home() {
         </h1>
         <button onClick={sendToWebSocket}>Send data to websocket!</button>
         <button onClick={closeWebSocket}>Close websocket!</button>
+        <div>
+          <h1>Bids</h1>
+          {bids && bids.map(bid => <p>{bid[0]}: {bid[1]}</p>)}
+        </div>
+        <div>
+          <h1>Asks</h1>
+          {asks && asks.map(ask => <p>{ask[0]}: {ask[1]}</p>)}
+        </div>
       </main>
     </div>
   );
